aebef2fe957f837eb92e0db14c5457b22739ab0a
max027 <saurabhwajage887@gmail.com>
2025-04-13 15:49:03 +0530

    added barbar

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -37,6 +37,26 @@ keymap("n", "<S-h>", ":bprevious<CR>", opts)
 keymap("n","<S-c>",":bdelete<CR>",opts)
 
 
+-- Re-order to previous/next
+keymap('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
+keymap('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
+
+-- Goto buffer in position...
+keymap('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
+keymap('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
+keymap('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
+keymap('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
+keymap('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
+keymap('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
+keymap('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
+keymap('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
+keymap('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
+keymap('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
+
+-- Magic buffer-picking mode
+keymap('n', '<C-p>',   '<Cmd>BufferPick<CR>', opts)
+keymap('n', '<C-s-p>', '<Cmd>BufferPickDelete<CR>', opts)
+
 -- Clear highlights
 keymap("n", "<leader>h", "<cmd>nohlsearch<CR>", opts)
 
